0587251cd918333f47f83773096c41ecae40d9ad
Cristian Velandia <velandia-95@hotmail.com>
2023-05-18 22:19:47 -0500

    Add ids to tables

--- Epidemiology/wwwroot/js/site.js
+++ Epidemiology/wwwroot/js/site.js
@@ -1,11 +1,9 @@
 ﻿$(document).ready(function () {
-    $("input[type='number']").on("change", function () {
-        let num = $(this).val();
-        if (!num.match(/^\d+/)) {
-            $(this).val(1);
-        }
-    });
+    manageTabMenus();
+    validateOnlyNumbers();
+});
 
+function manageTabMenus() {
     $('ul.tabs li a:first').addClass('active');
     $('.secciones article').hide();
     $('.secciones article:first').show();
@@ -17,4 +15,26 @@
         $(activeTab).show();
         return false;
     });
-});
+}
+
+function validateOnlyNumbers() {
+    $("input[type='number']").on("change", function () {
+        let num = $(this).val();
+        if (!num.match(/^\d+/)) {
+            $(this).val(1);
+        }
+    });
+}
+
+/*
+
+tbl1-info-cultivo-convencional-1,2,3,4
+tbl2-info-cultivo-pcr-1,2,3,4
+tbl3-descr-cultivo-convencional-1
+tbl4-descr-cultivo-pcr-1
+tbl5-medio-cultivo-1,2,3,4
+tbl6-info-talento-humano-cultivo-1,2,3
+tbl7-info-actividades-cultivo-1
+tbl8-tiempo-densidad-pruebas-1,2
+
+*/
